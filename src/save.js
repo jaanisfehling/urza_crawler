500d564ebf8521de87bf30a73ac439f2b3457101
jaanisfehling <jaanisfehling@gmail.com>
2023-01-10 00:07:30 +0100

    refactor into individual files

--- src/save.js
+++ src/save.js
@@ -1,12 +1,21 @@
-export async function saveToDB(headline, absolute_url, html) {
-    const { Client } = require("pg")
+export async function saveToDB(headline, datetime, absolute_url, html) {
+    const {Client} = require("pg")
     const client = new Client({
         host: "localhost",
         port: 5432,
         user: "postgres",
         password: "mysecretpassword",
-      })
+    })
     await client.connect()
-    
+
+    const text = "INSERT INTO article VALUES($1, $2, $3, $4) RETURNING *"
+    const values = [headline, datetime, absolute_url, html]
+    try {
+        const res = await client.query(text, values)
+        console.log(res.rows[0])
+    } catch (err) {
+        console.log(err.stack)
+    }
+
     await client.end()
 }
