bdcde1554b9f18dc7193b3e86e1cbdcac59c8eb6
jaanisfehling <jaanisfehling@gmail.com>
2023-04-05 22:54:05 +0200

    refactor, add header

--- app/src/main/java/urza_crawler/CrawlTask.java
+++ app/src/main/java/urza_crawler/CrawlTask.java
@@ -7,54 +7,75 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.time.Instant;
 import java.util.concurrent.Callable;
 
 import static urza_crawler.UrlUtils.getAbsoluteUrl;
 import static urza_crawler.UrlUtils.getBaseUrl;
 
 public class CrawlTask implements Callable<Boolean> {
-    public String listViewUrl;
-    public String articleSelector;
-    public String mostRecentArticleUrl;
+    String listViewUrl;
+    String articleSelector;
+    String mostRecentArticleUrl;
+    transient String siteName;
 
-    private void updateMostRecentArticleUrl() {
+    private void updateMostRecentArticleUrl(String newMostRecentArticleUrl) {
         String query = "UPDATE \"target\" SET most_recent_article_url=? WHERE list_view_url=?";
         try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:32768/", "postgres", "mysecretpassword")) {
             try (PreparedStatement stmt = conn.prepareStatement(query)) {
-                stmt.setString(1, mostRecentArticleUrl);
+                stmt.setString(1, newMostRecentArticleUrl);
                 stmt.setString(2, listViewUrl);
                 stmt.executeUpdate();
             } catch (SQLException e) {
-                System.out.println(e.getMessage());
+                System.out.println("Exception: " + e.getMessage());
             }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Exception: " + e.getMessage());
         }
     }
 
-    private Document request(String url) {
+    private Document request(String url, String referrerUrl) {
+
         try {
-            return Jsoup.connect(url).get();
+            Document doc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:111.0) Gecko/20100101 Firefox/111.0")
+                    .header("Accept-Language", "en-US,en;q=0.9")
+                    .referrer(referrerUrl)
+                    .timeout(3000)
+                    .ignoreContentType(true)
+                    .followRedirects(true)
+                    .get();
+            return doc;
         } catch (IOException e) {
-            System.out.println("Error fetching " + url);
+            System.out.println("Exception: " + e.getMessage());
             return null;
         }
     }
 
+    private boolean scrapeArticle(String url, String referrerUrl) {
+        Document articleDoc = request(url, referrerUrl);
+        if (articleDoc == null) {
+            return false;
+        }
+        String htmlContent = articleDoc.select("*").html();
+        CrawlResult result = new CrawlResult(url, siteName, htmlContent);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(result);
+        Main.pipelineClient.send(json);
+        return true;
+    }
+
     @Override
     public Boolean call() {
-        Document listViewDoc = request(listViewUrl);
-
+        Document listViewDoc = request(listViewUrl, "www.google.com");
 
         // Get the Name of the List View Site
         Element title = listViewDoc.select("title").first();
-        String siteName = (title == null) ? null : title.text();
+        siteName = (title == null) ? null : title.text();
 
         // Iterate over all headlines
         Elements headlines = listViewDoc.select(articleSelector);
@@ -62,26 +83,17 @@ public class CrawlTask implements Callable<Boolean> {
         for (Element headline : headlines) {
             String articleUrl = getAbsoluteUrl(getBaseUrl(listViewUrl), headline.attr("href"));
 
+            // If current Headline matches most recent article, we cancel
             if (articleUrl.equals(mostRecentArticleUrl) || headline.attr("href").equals(mostRecentArticleUrl)) {
                 return true;
             }
             else {
                 if (isFirstArticle) {
-                    mostRecentArticleUrl = articleUrl;
-                    updateMostRecentArticleUrl();
+                    updateMostRecentArticleUrl(articleUrl);
                     isFirstArticle = false;
                 }
-                Document articleDoc = null;
-                articleDoc = request(articleUrl);
-
-                // Create Crawl Result
-                String htmlContent = articleDoc.select("*").html();
-                CrawlResult result = new CrawlResult(articleUrl, siteName, htmlContent);
-
-                // Send scraped result to Pipeline
-                Gson gson = new Gson();
-                String json = gson.toJson(result);
-                Main.pipelineClient.send(json);
+                boolean result = scrapeArticle(articleUrl, listViewUrl);
+                // TODO: rotate proxies
             }
         }
         return true;
