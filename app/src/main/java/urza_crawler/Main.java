89629887b1c1f778bc00cb89dbc3d5552881cc46
jaanisfehling <jaanisfehling@gmail.com>
2023-04-10 23:39:29 +0200

    logging config setup

--- app/src/main/java/urza_crawler/Main.java
+++ app/src/main/java/urza_crawler/Main.java
@@ -2,12 +2,19 @@ package urza_crawler;
 
 import org.java_websocket.client.WebSocketClient;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Main {
+    public static Logger logger = Logger.getLogger("Urza Crawler");
     public static URI pipelineUri;
     public static URI queueUri;
     public static WebSocketClient pipelineClient;
@@ -15,8 +22,15 @@ public class Main {
     public static ExecutorService pool = Executors.newFixedThreadPool(4);
 
     public static void main(String[] args) throws URISyntaxException {
-        System.out.println("Running on JVM version " + System.getProperty("java.version"));
-        System.out.println("Number of Available Processors: " + Runtime.getRuntime().availableProcessors());
+        try {
+            LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
+        } catch (SecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        logger.setLevel(Level.FINE);
+
+        logger.log(Level.CONFIG, "Running on JVM version " + System.getProperty("java.version"));
+        logger.log(Level.CONFIG, "Number of Available Processors: " + Runtime.getRuntime().availableProcessors());
 
         pipelineUri = new URI("ws://localhost:8888");
         pipelineClient = new Client(pipelineUri);
